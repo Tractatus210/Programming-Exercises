02d13eb353d7c2033ad4e08aa7b5787a9e1265ae
Tractatus210 <67396070+Tractatus210@users.noreply.github.com>
2021-02-24 17:50:08 +0000

    Add files via upload
    Outputs the largest integer in a list of integers.

---
+++ Largest Numerical Element.py
@@ -0,0 +1,15 @@
+# Write a function that returns the largest element in a list.
+
+#Largest integer in a list of integers.
+
+list = [5, 1, 3, 2, 7]
+
+def listOrder(list):
+    # Use sorted instead of sort to create a new list, leaving the original intact.
+    # Set reverse to True to display largest value first.
+    sortedList = sorted(list, reverse=True)
+    print(sortedList[0])
+
+listOrder(list)
+
+
