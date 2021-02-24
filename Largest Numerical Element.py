2818a0b6d8836061423ed8c1465220ac4f11d5a2
Tractatus210 <67396070+Tractatus210@users.noreply.github.com>
2021-02-24 17:59:18 +0000

    Update Largest Numerical Element.py

--- Largest Numerical Element.py
+++ Largest Numerical Element.py
@@ -2,14 +2,11 @@
 
 #Largest integer in a list of integers.
 
-list = [5, 1, 3, 2, 7]
-
 def listOrder(list):
     # Use sorted instead of sort to create a new list, leaving the original intact.
     # Set reverse to True to display largest value first.
     sortedList = sorted(list, reverse=True)
     print(sortedList[0])
 
-listOrder(list)
 
 
