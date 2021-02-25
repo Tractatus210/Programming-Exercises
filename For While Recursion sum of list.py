bd41f3e5cfecd7b8b5bbbcf0dba00c72f6a0e1d6
Tractatus210 <67396070+Tractatus210@users.noreply.github.com>
2021-02-25 17:34:38 +0000

    Add files via upload
    Three functions to calculate the sum of numbers in a list, using a for loop, while loop and recursion.

---
+++ For While Recursion sum of list.py
@@ -0,0 +1,44 @@
+# Write three functions that compute the sum of the numbers in a list:
+# using a for-loop, a while-loop and recursion.
+
+a = [5, 4, 3, 2, 1]
+
+# For loop
+
+def forSum(a):
+    total = 0
+    for n in a:
+        total = total + n
+    print(total)
+
+# While Loop
+
+def whileSum(a):
+    n = 0
+    total = 0
+    while n < len(a):
+        total = total + a[n]
+        n += 1
+    print(total)
+
+# Recursion
+
+def recurSum(a):
+    if len(a) == 1:
+        return a[0]
+    else:
+        return a[0] + recurSum(a[1:])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
