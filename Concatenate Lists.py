91663e548ad6393b8544b6900331e67d3664599b
Tractatus210 <67396070+Tractatus210@users.noreply.github.com>
2021-02-25 20:05:44 +0000

    Add files via upload
    A function to concatenate two lists.

---
+++ Concatenate Lists.py
@@ -0,0 +1,7 @@
+# Write a function that concatenates two lists. [a,b,c], [1,2,3] → [a,b,c,1,2,3]
+
+# Define function with two arguments to pass lists through.
+def concList(a, b):
+    # Create variable to store concatenated list. Output the value of this variable.
+    c = a + b
+    print(c)
